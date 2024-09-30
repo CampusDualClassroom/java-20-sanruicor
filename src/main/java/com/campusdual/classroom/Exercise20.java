89e6517502fb3be9b305db276125329e15508c96
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-09-30 11:10:05 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,19 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        List <Person> lista = new ArrayList<>();
+        lista.add(new Person("John", "Smith"));
+        lista.add(new Teacher("María", "Montessori", "Educación"));
+        lista.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        lista.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
 
+        return lista;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person person : stringList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
